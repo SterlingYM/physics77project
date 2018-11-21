5004d23df6b17773d4648f683b34edf10839a070
SterlingYM <sterling.astro@berkeley.edu>
2018-11-21 14:31:04 -0800

    11/21/2018

---
+++ function_prototypes.py
@@ -0,0 +1,37 @@
+# function ptototypes here
+
+
+def data_read():
+
+
+def dist():
+
+
+def force_ij():
+
+
+def components():
+
+
+def net_force():
+
+
+def acccel():
+
+
+def pos():
+
+
+def vel():
+
+
+def starloop():
+
+
+def time_development():
+
+
+def animate():
+
+
+
