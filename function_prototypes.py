64fe945977a2ec87949e27d1c95607428d8021ce
SterlingYM <sterlingym.astro@berkeley.edu>
2018-11-28 00:41:54 -0800

    11/28/2018

--- function_prototypes.py
+++ function_prototypes.py
@@ -1,5 +1,10 @@
 # constants
-G = 
+G = 6.67408 * 10**(-11) #[m^3 * kg^(-1) * s^(-2)]
+
+# parameters
+csv_filename = 'csv_filename.csv'
+dt = 5000 #[s]
+t_max = 10**100
 
 
 
@@ -42,16 +47,12 @@ def net_force(starlist,i):
     # resulting value after for loop should be the net force on star[i]
     net_Fx, net_Fy = 0, 0
     m1, x1, y1 = starlist[i][1], starlist[i][2], starlist[i][3]
-    def helper(j):
-        if j == len(starlist):
-            return net_Fx, net_Fy
+    for j in range(len(starlist)):
         if j != i:
             m2, x2, y2 = starlist[j][1], starlist[j][2], starlist[j][3]
             d = dist(x1,x2,y1,y2)
             net_Fx, net_Fy += components(force_ij(m1,m2,d),x1,x2,y1,y2)
-        helper(j+1)
-    helper(0)
-
+    return net_Fx, net_Fy
 
 def acccel(mass,Fx,Fy):
     # calculates acceleration from net force ans mass
@@ -77,16 +78,31 @@ def vel(starlist,i,ax,ay,dt):
     return v_x, v_y
 
 
-def starloop():
+def starloop(starlist):
     # repeats calculation of new starlist for star[i]
     # calls net_force(), accel(), pos(), vel() within a loop for i
-    
-    return starlist_next
+    new_starlist = []
+    for i in range(len(starlist):
+        mass = starlist[i][1]
+        Fx,Fy = net_force(starlist,i)
+        ax,ay = accel(mass,Fx,Fy)
+        x_new, y_new   = pos(starlist,i,ax,ay,dt)
+        vx_new, vy_new = vel(starlist,i,ax,ay,dt)
+        new_starlist.append([starlist[i][0],mass,x_new,y_new,vx_new,vy_new])
+    return new_starlist
 
-
-def time_development():
+def time_development(initial_list,dt,t_max):
     # calls starloop() and stacks result on 'gal_hist'
     # repeats above for given time length
+    gal_hist = []
+    t = 0
+    for k in range(int(t_max/dt)):
+        if k == 0:
+            starlist = starloop(initial_list)
+            gal_hist.append([t,starlist])
+        else:
+            gal_hist.append(starloop([t,gal_hist[k-1][1]]))
+        t += dt
     return gal_hist
 
 def animate():
@@ -98,6 +114,12 @@ def animate():
     # need to integrate with other functions before continuing 
     
     #edit attempt
+
+
+#### main ####
+initial_list = dataread(csv_filename)
+gal_hist = time_development(initial_list,dt,t_max)
+animate(gal_hist)
 
 
 
